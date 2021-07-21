535453b2718c7e7de02ba25db967d5aa025cd404
Max Turon <maxx@turon.fr>
2021-07-21 15:13:26 +0200

    Ajout sauce réagit et envoie uniquement l'id dans la BDD.

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,11 +1,16 @@
 const Sauce = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce);
-    console.log(sauceObject)
+    delete req.body._id;
     const sauce = new Sauce({
-        ...sauceObject,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        mainPepper: req.body.mainPepper,
+        heat: req.body.heat,
     });
+    console.log(sauce)
     sauce.save()
         .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
         .catch(error => res.status(400).json({ error }));
